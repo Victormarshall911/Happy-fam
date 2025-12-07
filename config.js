a1f18102a99ff1519a39257c9c756672e9d308c5
Victormarshall911 <favourjames1977@gmail.com>
2025-12-07 16:30:13 +0100

    Stop tracking config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    SUPABASE_URL: "https://yewqtqalasmospplgdwb.supabase.co",
-    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlld3F0cWFsYXNtb3NwcGxnZHdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMTU0NjgsImV4cCI6MjA4MDY5MTQ2OH0.__gpV6kX8R0rTv0y_vqNeOgqLEFE9q_hRSS1WDdkw0M"
-};
