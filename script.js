d1efc80c605756f1f23f6230bd18df0492415574
Victormarshall911 <favourjames1977@gmail.com>
2025-12-11 08:52:13 +0100

    Updated users

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// --- CONFIGURATION ---
-// Ensure config.js is loaded before this file in index.html
-const supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
+const SUPABASE_URL = "https://yewqtqalasmospplgdwb.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlld3F0cWFsYXNtb3NwcGxnZHdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMTU0NjgsImV4cCI6MjA4MDY5MTQ2OH0.__gpV6kX8R0rTv0y_vqNeOgqLEFE9q_hRSS1WDdkw0M";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // --- NAVIGATION ---
 function showSection(id) {
